494e7fbbd7da87cabee04c23fe32adb18b8ac9bf
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-15 23:12:40 +0200

    productList & dat user9

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
-import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 function App() {
-    const {onToggleButton, tg} = useTelegram();
+    const { tg } = useTelegram(); 
 
     useEffect(() => {
         tg.ready();
-    }, [])
-
+    }, [tg]);
     return (
         <div className="App">
             <Header />
             <Routes>
-                <Route index element={<ProductList />}/>
-                <Route path={'form'} element={<Form />}/>
+                <Route index element={<ProductList />} />
+                <Route path="form" element={<Form />} />
             </Routes>
         </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,32 @@
+
 import './Header.css';
 
 const tg = window.Telegram.WebApp || {};
 
 export function useTelegram() {
-    const onToggleButton = () => {
-        if (tg.MainButton?.isVisible) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    };
+  const onToggleButton = () => {
+    if (tg.MainButton?.isVisible) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  };
 
-    return {
-        tg,
-        onToggleButton,
-        user: tg.initDataUnsafe?.user || null,
-    };
+  return {
+    tg,
+    onToggleButton,
+    user: tg.initDataUnsafe?.user || null,
+  };
 }
 
-
 const Header = () => {
-    const { user } = useTelegram();
+  const { user } = useTelegram();
 
-    return (
-        <div className="header">
-            <h1>Welcome {user?.first_name || 'Guest'}</h1>
-        </div>
-    );
+  return (
+    <div className="header">
+      <h1>Welcome {user?.first_name || 'Guest'}</h1>
+    </div>
+  );
 };
 
 export default Header;
