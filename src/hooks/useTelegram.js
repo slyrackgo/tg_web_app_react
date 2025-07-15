5bc08aac7b00c6bf8c0f06a05e8ac82a6cd52c13
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-15 23:19:16 +0200

    productList & dat user10

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,17 +1,23 @@
 const tg = window.Telegram.WebApp;
 
 export function useTelegram() {
+
+    const onClose = () => {
+        tg.close()
+    }
+
     const onToggleButton = () => {
-        if (tg.MainButton.isVisible) {
+        if(tg.MainButton.isVisible) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
         }
-    };
+    }
 
     return {
-        tg,
+        onClose,
         onToggleButton,
+        tg,
         user: tg.initDataUnsafe?.user,
-    };
+    }
 }
