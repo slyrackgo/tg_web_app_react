64b25b78e80997765b3f33b69f455ab7ad34b9a8
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-15 23:04:50 +0200

    productList & dat user6

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import './Form.css';
-import { useTelegram } from "../../hooks/useTelegram";
+import {useTelegram} from "../../hooks/useTelegram";
+
 
 const Form = () => {
     const [city, setCity] = useState('');
