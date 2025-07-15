47ebcb65d77f9d45481447476a78b71e6eebd242
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-15 23:07:35 +0200

    productList & dat user7

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,19 @@
+import React from 'react';
+import Button from "../Button/Button";
+import {useTelegram} from "../../hooks/useTelegram";
 import './Header.css';
-const tg = window.Telegram.WebApp || {};
 
-export function useTelegram() {
-    const onToggleButton = () => {
-        if (tg.MainButton?.isVisible) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    };
+const Header = () => {
+    const {user, onClose} = useTelegram();
 
-    return {
-        tg,
-        onToggleButton,
-        user: tg.initDataUnsafe?.user || null,
-    };
-}
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>Закрыть</Button>
+            <span className={'username'}>
+                {user?.username}
+            </span>
+        </div>
+    );
+};
+
+export default Header;
