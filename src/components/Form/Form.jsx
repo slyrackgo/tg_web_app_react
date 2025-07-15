9291393000c22bed0a6aeebadf358910ca67c63b
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-15 22:51:35 +0200

    productList & dat user2

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,41 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Form.css';
-import {useTelegram} from "../../hooks/useTelegram";
+import { useTelegram } from '../../hooks/useTelegram';
 
 const Form = () => {
     const [city, setCity] = useState('');
+    const [address, setAddress] = useState('');
+    const [phone, setPhone] = useState('');
+    const { tg } = useTelegram();
 
     useEffect(() => {
-            tg.MainButton.setParams({
-                text: 'Отправить данные'
-            })
-        }, [])
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        });
+    }, [tg]);
 
-    //do not allow to send empty data    
     useEffect(() => {
-        if(!street || !country) {
+        if (!city || !address || !phone) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
         }
-    }, [country, street])
+    }, [city, address, phone, tg]);
 
     return (
         <div>
             <h3>Введите ваши данные</h3>
-            <input className='input' 
-            type="text" 
-            placeholder='Город'
+            <input
+                className="input"
+                type="text"
+                placeholder="Город"
+                value={city}
+                onChange={e => setCity(e.target.value)}
             />
             <input
-                className={'input'}
+                className="input"
                 type="text"
-                placeholder={'Улица'}
-                value={street}
-                onChange={onChangeStreet}
+                placeholder="Адрес"
+                value={address}
+                onChange={e => setAddress(e.target.value)}
+            />
+            <input
+                className="input"
+                type="tel"
+                placeholder="Номер телефона"
+                value={phone}
+                onChange={e => setPhone(e.target.value)}
             />
         </div>
-    )
-}
+    );
+};
 
 export default Form;
