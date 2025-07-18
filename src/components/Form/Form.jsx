a9cefc6048d0fd279eaaa67b6e00c1bb97452dc3
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-18 18:13:23 +0200

    netlifyfix111

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,57 +1,58 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import { useTelegram } from '../../hooks/useTelegram';
+import React, {useEffect, useState} from 'react';
 import './Form.css';
+import {useTelegram} from "../../hooks/useTelegram";
+
 
 const Form = () => {
-  const [city, setCity] = useState('');
-  const [address, setAddress] = useState('');
-  const [phone, setPhone] = useState('');
-  const { tg } = useTelegram();
-  const MainButton = tg.MainButton;
+    const [city, setCity] = useState('');
+    const [street, setStreet] = useState('');
+    const [phone, setPhone] = useState('');
+    const { tg } = useTelegram();
 
-  const onSendData = useCallback(() => {
-    const data = { city, address, phone };
-    tg.sendData(JSON.stringify(data));
-  }, [city, address, phone, tg]);
+    const onChangeCity = (e) => zsetCity(e.target.value);
+    const onChangeStreet = (e) => setStreet(e.target.value);
+    const onChangePhone = (e) => setPhone(e.target.value);
 
-  useEffect(() => {
-    MainButton.setParams({ text: 'Отправить данные' });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        });
+    }, [tg]);
 
-  useEffect(() => {
-    MainButton.onClick(onSendData);
-    MainButton.show();
-    return () => MainButton.offClick(onSendData);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    useEffect(() => {
+        if (!city || !street || !phone) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [city, street, phone, tg]);
 
-  return (
-    <div className="form">
-      <h3>Введите ваши данные</h3>
-      <input
-        className="input"
-        type="text"
-        placeholder="Город"
-        value={city}
-        onChange={e => setCity(e.target.value)}
-      />
-      <input
-        className="input"
-        type="text"
-        placeholder="Адрес"
-        value={address}
-        onChange={e => setAddress(e.target.value)}
-      />
-      <input
-        className="input"
-        type="tel"
-        placeholder="Номер телефона"
-        value={phone}
-        onChange={e => setPhone(e.target.value)}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <h3>Введите ваши данные</h3>
+            <input
+                className='input'
+                type="text"
+                placeholder='Город'
+                value={city}
+                onChange={onChangeCity}
+            />
+            <input
+                className='input'
+                type="text"
+                placeholder='Адрес'
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <input
+                className='input'
+                type="tel"
+                placeholder='Номер телефона'
+                value={phone}
+                onChange={onChangePhone}
+            />
+        </div>
+    );
 };
 
 export default Form;
