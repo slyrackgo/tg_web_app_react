83db64d1f40598824b9a8ae0b73ae6b94505750b
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-18 17:16:13 +0200

    netlify3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import './App.css';
-import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
-import Header from "./components/Header/Header";
-import {Route, Routes} from 'react-router-dom'
-import ProductList from "./components/ProductList/ProductList";
-import Form from "./components/Form/Form";
+import { useEffect } from 'react';
+import { useTelegram } from './hooks/useTelegram';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 function App() {
-    const {onToggleButton, tg} = useTelegram();
+  const { tg } = useTelegram();
 
-    useEffect(() => {
-        tg.ready();
-    }, [])
+  useEffect(() => {
+    tg.ready();
+  }, [tg]);
 
-    return (
-        <div className="App">
-            <Header />
-            <Routes>
-                <Route index element={<ProductList />}/>
-                <Route path={'form'} element={<Form />}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/form" element={<Form />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
