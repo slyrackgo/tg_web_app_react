72d3817a55ff323fa1410a9f467534f28dc02e86
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-18 17:17:22 +0200

    ai fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,33 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import './Form.css';
-import { useTelegram } from '../../hooks/useTelegram';
+import {useTelegram} from "../../hooks/useTelegram";
+
 
 const Form = () => {
-  const [city, setCity] = useState('');
-  const [street, setStreet] = useState('');
-  const [phone, setPhone] = useState('');
-  const { tg } = useTelegram();
+    const [city, setCity] = useState('');
+    const [street, setStreet] = useState('');
+    const [phone, setPhone] = useState('');
+    const { tg } = useTelegram();
 
-  useEffect(() => {
-    tg.MainButton.setParams({ text: 'Отправить данные' });
-  }, [tg]);
+    const onChangeCity = (e) => setCity(e.target.value);
+    const onChangeStreet = (e) => setStreet(e.target.value);
+    const onChangePhone = (e) => setPhone(e.target.value);
 
-  useEffect(() => {
-    if (!city || !street || !phone) {
-      tg.MainButton.hide();
-    } else {
-      tg.MainButton.show();
-    }
-  }, [city, street, phone, tg]);
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        });
+    }, [tg]);
 
-  return (
-    <div className="form">
-      <h3>Введите ваши данные</h3>
-      <input value={city} onChange={e => setCity(e.target.value)} placeholder="Город" className="input" />
-      <input value={street} onChange={e => setStreet(e.target.value)} placeholder="Адрес" className="input" />
-      <input value={phone} onChange={e => setPhone(e.target.value)} placeholder="Номер телефона" className="input" type="tel" />
-    </div>
-  );
+    useEffect(() => {
+        if (!city || !street || !phone) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [city, street, phone, tg]);
+
+    return (
+        <div>
+            <h3>Введите ваши данные</h3>
+            <input
+                className='input'
+                type="text"
+                placeholder='Город'
+                value={city}
+                onChange={onChangeCity}
+            />
+            <input
+                className='input'
+                type="text"
+                placeholder='Адрес'
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <input
+                className='input'
+                type="tel"
+                placeholder='Номер телефона'
+                value={phone}
+                onChange={onChangePhone}
+            />
+        </div>
+    );
 };
 
 export default Form;
