0b9d37608b238110a9fa23a1ccd1db474884cd25
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-18 19:24:50 +0200

    form1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 
 
+// import './App.css';
+// import React, { useEffect } from "react";
+// import { useTelegram } from "./hooks/useTelegram";
+// import Header from "./components/Header/Header";
+// import { Route, Routes } from 'react-router-dom';
+// import ProductList from "./components/ProductList/ProductList";
+// import Form from "./components/Form/Form";
+
+// function App() {
+//     const { tg } = useTelegram(); 
+
+//     useEffect(() => {
+//         tg.ready();
+//     }, [tg]);
+//     return (
+//         <div className="App">
+//             <Header />
+//             <Routes>
+//                 <Route index element={<ProductList />} />
+//                 <Route path="form" element={<Form />} />
+//             </Routes>
+//         </div>
+//     );
+// }
+
+// export default App;
 import './App.css';
-import React, { useEffect } from "react";
-import { useTelegram } from "./hooks/useTelegram";
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 function App() {
-    const { tg } = useTelegram(); 
+    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
-    }, [tg]);
+    }, [])
+
     return (
         <div className="App">
             <Header />
             <Routes>
-                <Route index element={<ProductList />} />
-                <Route path="form" element={<Form />} />
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
             </Routes>
         </div>
     );
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,3 +1,26 @@
+// const tg = window.Telegram.WebApp;
+
+// export function useTelegram() {
+
+//     const onClose = () => {
+//         tg.close()
+//     }
+
+//     const onToggleButton = () => {
+//         if(tg.MainButton.isVisible) {
+//             tg.MainButton.hide();
+//         } else {
+//             tg.MainButton.show();
+//         }
+//     }
+
+//     return {
+//         onClose,
+//         onToggleButton,
+//         tg,
+//         user: tg.initDataUnsafe?.user,
+//     }
+// }
 const tg = window.Telegram.WebApp;
 
 export function useTelegram() {
@@ -19,5 +42,6 @@ export function useTelegram() {
         onToggleButton,
         tg,
         user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
     }
 }
