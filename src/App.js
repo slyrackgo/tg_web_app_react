02c5cbdcc4c71cc10c8b103be1d683f09753e736
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-18 17:23:52 +0200

    fallback101

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import './App.css';
-import { useEffect } from 'react';
-import { useTelegram } from './hooks/useTelegram';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import { Route, Routes } from 'react-router-dom';
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
-  const { tg } = useTelegram();
+    const { tg } = useTelegram(); 
 
-  useEffect(() => {
-    tg.ready();
-  }, [tg]);
-
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/form" element={<Form />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    useEffect(() => {
+        tg.ready();
+    }, [tg]);
+    return (
+        <div className="App">zz
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />} />
+                <Route path="form" element={<Form />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
