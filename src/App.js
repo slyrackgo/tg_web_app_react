d412823f1e75435ff43c5b0f4cb93775428852d7
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-18 19:11:24 +0200

    useEffectFixed

--- src/App.js
+++ src/App.js
@@ -11,17 +11,44 @@ function App() {
 
     useEffect(() => {
         tg.ready();
-    }, [])
+    }, [tg])
 
     return (
         <div className="App">
             <Header />
             <Routes>
                 <Route index element={<ProductList />}/>
-                <Route path={'form'} element={<Form />}/>
+                <Route path="form" element={<Form />}/>
             </Routes>
         </div>
     );
 }
 
 export default App;
+
+// import './App.css';
+// import React, { useEffect } from "react";
+// import { useTelegram } from "./hooks/useTelegram";
+// import Header from "./components/Header/Header";
+// import { Route, Routes } from 'react-router-dom';
+// import ProductList from "./components/ProductList/ProductList";
+// import Form from "./components/Form/Form";
+
+// function App() {
+//     const { tg } = useTelegram(); 
+
+//     useEffect(() => {
+//         tg.ready();
+//     }, [tg]);
+//     return (
+//         <div className="App">
+//             <Header />
+//             <Routes>
+//                 <Route index element={<ProductList />} />
+//                 <Route path="form" element={<Form />} />
+//             </Routes>
+//         </div>
+//     );
+// }
+
+// export default App;
