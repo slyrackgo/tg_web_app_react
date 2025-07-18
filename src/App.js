f7936062f22c485d981dd899da1b483b759e99d6
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-18 16:41:46 +0200

    product list fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
-import { useEffect } from "react";
-import { useTelegram } from "./hooks/useTelegram";
-import Header from "./components/Header/Header";
-import { Route, Routes } from 'react-router-dom';
-import ProductList from "./components/ProductList/ProductList";
-import Form from "./components/Form/Form";
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useTelegram } from './hooks/useTelegram';
+import Form from './components/Form/Form';
+import ProductList from './components/ProductList/ProductList';
 
 function App() {
-    const { tg } = useTelegram(); 
+    const { tg } = useTelegram();
 
     useEffect(() => {
         tg.ready();
     }, [tg]);
+
     return (
         <div className="App">
-            <Header />
-            <Routes>
-                <Route index element={<ProductList />} />
-                <Route path="form" element={<Form />} />
-            </Routes>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<ProductList />} />
+                    <Route path="/form" element={<Form />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
