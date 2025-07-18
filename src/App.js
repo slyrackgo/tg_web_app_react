6e7a678d1997e7a912a94a00e6b6e201615c4865
Atai Ibragimov <aibragimov2003@bk.ru>
2025-07-18 18:29:12 +0200

    my fix101

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
@@ -12,17 +12,14 @@ function App() {
     useEffect(() => {
         tg.ready();
     }, [tg]);
-
     return (
-        <Router>
-            <div className="App">
-                <Header />
-                <Routes>
-                    <Route index element={<ProductList />} />
-                    <Route path="form" element={<Form />} />
-                </Routes>
-            </div>
-        </Router>
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />} />
+                <Route path="form" element={<Form />} />
+            </Routes>
+        </div>
     );
 }
 
